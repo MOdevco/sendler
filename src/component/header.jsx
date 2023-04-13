95121ab5f2ce47c13b6ce79fa6549cab0014809c
salom <salom@gmail.com>
2023-04-13 17:10:18 +0500

    added

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -3,7 +3,7 @@ import { Logo, Send } from "../images/img"
 
 const Header = () => {
   return (
-    <div className="headerShadow">
+    <div className="headerShadow w-full">
 
         <div className="bg-[#2F2F2F]">
             <div className="max-w-[1280px] mx-auto">
@@ -12,7 +12,7 @@ const Header = () => {
                         <img src={Logo} alt="" />
                         <h1 className="text-[32px] font-semibold">SENDLER</h1>
                     </div>
-                    <div>
+                    <div className="md:block hidden">
                         <a href="https://t.me/modevco" target="_blank"><img src={Send} className="absolute z-20 top-2" alt="" /></a>
                     </div>
                 </div>
